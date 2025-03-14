6c358c33fc304af9d314b9bcf3d242f6383d273b
Blake Baxter <blake.bax@gmail.com>
2025-03-13 19:19:00 -0500

    updates for animations

---
+++ app/card/FlipperText.tsx
@@ -0,0 +1,78 @@
+'use client';
+
+import React, { useState } from 'react';
+import { useSpring, a, animated } from '@react-spring/web';
+import { Verb } from './verbs';
+
+import styles from './styles.module.css';
+import { Button } from '@/components/ui/button';
+
+type CardProps = {
+  items: Verb[];
+};
+
+type VerbWithShown = Verb & {
+  shown?: boolean;
+};
+
+export default function FlipperText(props: CardProps) {
+  const showItems = props.items.map((item) => ({ ...item, shown: false }));
+  const [item, setItem] = useState<Verb>(showItems[0]);
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [flipped, set] = useState(false);
+  const { transform, opacity } = useSpring({
+    opacity: flipped ? 1 : 0,
+    transform: `perspective(600px) rotateX(${flipped ? 180 : 0}deg)`,
+    config: { mass: 5, tension: 500, friction: 80 },
+  });
+  const sharedStyles = { opacity, transform };
+  const sharedClassNames = `${styles.c} ${styles.shrink} px-2 flex text-center justify-center items-center card text-slate-900 text-4xl`;
+
+  function nextItem() {
+    console.log('nextItem');
+
+    if (currentIndex === showItems.length) {
+      setItem(showItems[0]);
+    } else {
+      const nextIndex = currentIndex + 1;
+      setItem(showItems[nextIndex]);
+      setCurrentIndex(nextIndex);
+    }
+  }
+
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center mt-4">
+        <div className={styles.absolute_wrapper}>
+          <div
+            className={styles.container}
+            onClick={() => set((state) => !state)}
+          >
+            <animated.div
+              className={`${sharedClassNames} bg-slate-100`}
+              style={{ ...sharedStyles, opacity: opacity.to((o) => 1 - o) }}
+            >
+              {item.english}
+            </animated.div>
+            <animated.div
+              className={`${sharedClassNames} bg-green-100`}
+              style={{ ...sharedStyles, rotateX: '180deg' }}
+            >
+              {item.spanish}
+            </animated.div>
+          </div>
+        </div>
+        <div
+          className={`${styles.absolute_wrapper} flex justify-between w-full mt-2`}
+        >
+          <Button className="mt-2" onClick={() => console.log('clicked')}>
+            Back
+          </Button>
+          <Button className="mt-2" onClick={() => nextItem()}>
+            Next
+          </Button>
+        </div>
+      </div>
+    </>
+  );
+}
---
+++ app/card/verbs.ts
@@ -0,0 +1,37 @@
+export type Verb = {
+  spanish: string;
+  english: string;
+};
+
+export default [
+  { spanish: 'acercarse a', english: 'to approach' },
+  { spanish: 'acostumbrarse a', english: 'to get used to' },
+  { spanish: 'aficionarse a', english: 'to become fond of' },
+  { spanish: 'asemejarse a', english: 'to resemble, look like' },
+  { spanish: 'asistir a', english: 'to attend' },
+  { spanish: 'asomarse a', english: 'to appear at' },
+  { spanish: 'cuidar a', english: 'to take care of' },
+  { spanish: 'dar a', english: 'to face, overlook' },
+  { spanish: 'dedicarse a', english: 'to devote oneself to' },
+  { spanish: 'dirigirse a', english: 'to go toward, address' },
+  { spanish: 'echar la culpa a', english: 'to blame' },
+  { spanish: 'entrar a', english: 'to enter' },
+  { spanish: 'equivaler a', english: 'to equal, be equivalent to' },
+  { spanish: 'estar sentado a', english: 'to be seated at' },
+  { spanish: 'ir a', english: 'to go to' },
+  { spanish: 'jugar a', english: 'to play (a game, sport)' },
+  { spanish: 'llamar a', english: 'to call, telephone' },
+  { spanish: 'llegar a', english: 'to arrive at/in' },
+  { spanish: 'oler a', english: 'to smell like' },
+  { spanish: 'paracerse a', english: 'to look like' },
+  { spanish: 'querer a', english: 'to love' },
+  { spanish: 'resistirse a', english: 'to resist' },
+  { spanish: 'responder a', english: 'to answer' },
+  { spanish: 'saber a', english: 'to taste like' },
+  { spanish: 'ser aficionado a', english: 'to be a fan of' },
+  { spanish: 'sonar a', english: 'to sound like' },
+  { spanish: 'subir a', english: 'to board, get on/into' },
+  { spanish: 'tocar a', english: 'to knock on' },
+  { spanish: 'tocarle a ___', english: 'to be ___’s turn' },
+  { spanish: 'viajar a', english: 'to travel to (a place)' },
+] as Verb[];
