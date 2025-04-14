7fdaa151d4f77e47b30ac836b4002e02c39dae28
Blake Baxter <blake.bax@gmail.com>
2025-04-14 09:02:48 -0500

    cleanup

--- app/test/Flipper.tsx
+++ app/test/Flipper.tsx
@@ -1,20 +1,21 @@
 'use client';
 
 import React, { useState } from 'react';
-import { useSpring, a } from '@react-spring/web';
+// import { useSpring, a } from '@react-spring/web';
 
 import styles from './styles.module.css';
 
 export default function Flipper() {
   const [flipped, set] = useState(false);
-  const { transform, opacity } = useSpring({
-    opacity: flipped ? 1 : 0,
-    transform: `perspective(600px) rotateX(${flipped ? 180 : 0}deg)`,
-    config: { mass: 5, tension: 500, friction: 80 },
-  });
+  // const { transform, opacity } = useSpring({
+  //   opacity: flipped ? 1 : 0,
+  //   transform: `perspective(600px) rotateX(${flipped ? 180 : 0}deg)`,
+  //   config: { mass: 5, tension: 500, friction: 80 },
+  // });
   return (
     <div className={styles.container} onClick={() => set((state) => !state)}>
-      <a.div
+      TODO!
+      {/* <a.div
         className={`${styles.c} ${styles.back}`}
         style={{ opacity: opacity.to((o) => 1 - o), transform }}
       />
@@ -25,7 +26,7 @@ export default function Flipper() {
           transform,
           rotateX: '180deg',
         }}
-      />
+      /> */}
     </div>
   );
 }
